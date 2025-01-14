003a5b972d1de11c271128b66bc3db629e5a62f7
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-01-14 12:34:48 +0100

    add: end es. 1

--- main.js
+++ main.js
@@ -10,6 +10,21 @@
 //      - se mail presente: utente può accedere alla festa
 //      - altrimenti: utente non può accedere alla festa
 
+const festaInvitatiMail = ['lorenzo@gmail.com', 'alex@gmail.com', 'franco@gmail.com'];
+let isUtenteInvitato = false;
+let mailUtente = prompt('Inserisci la tua email per verificare se sei stato invitato alla festa', 'franco@gmail.com');
+
+console.log(`email inserita: ${mailUtente}`)
+
+for (i = 0; (i < festaInvitatiMail.length); i++) {
+    if(mailUtente == festaInvitatiMail[i]) {
+        isUtenteInvitato = true;
+        
+    }
+}
+
+isUtenteInvitato == true ? console.log('Puoi accedere alla festa') : console.log('Mi dispiace, non sei stato invitato');
+
 
 
 
