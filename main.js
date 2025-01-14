2762a6a3a6e66db6ad0abde23232c4b5b244d154
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-01-14 11:56:28 +0100

    add: es. 1 workflow

--- main.js
+++ main.js
@@ -2,7 +2,13 @@
 
 // esercizio mail
 
+// workflow
 
+// 1. creare lista di email predefinita
+// 2. creare var con email inserita dall'utente
+// 3. controllo se email utente presente nella lista
+//      - se mail presente: utente può accedere alla festa
+//      - altrimenti: utente non può accedere alla festa
 
 
 
